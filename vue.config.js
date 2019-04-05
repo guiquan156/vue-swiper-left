ddf22fd7f81f887520595326747cdbde504bab85
guiquan156 <guiquan156@163.com>
2019-04-06 00:53:36 +0800

    打包及开发脚本

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+const path = require('path');
+const HtmlWebpackPlugins = require('html-webpack-plugin');
 
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new HtmlWebpackPlugins({
+        template: path.resolve(__dirname, 'demo/index.html')
+      })
+    ]
+  }
 };
