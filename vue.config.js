d278dd696fb4cf041d370545d81738c3bd965540
guiquan156 <guiquan156@163.com>
2019-04-06 00:59:38 +0800

    使用更加好的方式修改html模板

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
-const HtmlWebpackPlugins = require('html-webpack-plugin');
 
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new HtmlWebpackPlugins({
-        template: path.resolve(__dirname, 'demo/index.html')
-      })
-    ]
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].template = path.resolve(__dirname, 'demo/index.html');
+        return args;
+      });
   }
 };
