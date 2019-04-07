62f4096183a578abd451bddb8419e3ab37cf1514
guiquan156 <guiquan156@163.com>
2019-04-07 20:26:27 +0800

    add event

--- demo/app.vue
+++ demo/app.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="app">
-    <swiper-left class="item" v-for="item in list" :key="item.id" :defaultSlid="true">
+    <swiper-left class="item" v-for="item in list" :key="item.id">
       <div class="content">this is the {{item.id}} item</div>
-      <template slot="left">
+      <template slot="right">
         <span class="delete">delete</span>
       </template>
     </swiper-left>
@@ -37,9 +37,9 @@ export default {
 
 .item {
   width: 100%;
-  height: 60px;
+  height: 70px;
   border-bottom: 1px solid #eee;
-  line-height: 35px;
+  line-height: 45px;
 }
 
 .content {
@@ -53,7 +53,7 @@ export default {
   color: #fff;
   background: #ed3f3f;
   text-align: center;
-  line-height: 59px;
+  line-height: 69px;
 }
 </style>
 
--- src/swiper-left.vue
+++ src/swiper-left.vue
@@ -3,23 +3,29 @@
     <div class="swp-item"
       :style="swiperStyle"
       ref="swp">
-      <div class="swp-right">
+      <div class="swp-left">
         <slot></slot>
       </div>
-      <div class="swp-left" :style="leftStyle">
-        <slot name="left"></slot>
+      <div class="swp-right" :style="leftStyle">
+        <slot name="right"></slot>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { parse } from 'path';
 export default {
   name: 'SwiperLeft',
   props: {
     width: {
       default () {
-        return '25%';
+        return 100;
+      }
+    },
+    minWidth: {
+      default () {
+        return 80;
       }
     },
     defaultSlid: {
@@ -36,23 +42,86 @@ export default {
   computed: {
     swiperStyle () {
       return {
-        transform: `translateX(${this.slid ? '-' + this.width : 0})`
+        transform: `translateX(${this.slid ? '-' + this.width : 0}px)`
       }
     },
     leftStyle () {
       return {
-        width: this.width,
-        marginRight: '-' + this.width,
+        width: this.width + 'px',
+        marginRight: `-${this.width}px`,
       }
     }
   },
   mounted () {
     const $swp = this.$refs.swp;
+    let startTouch = null;
+    let startStamp = 0;
+    let isHor = false; // 是否水平移动
+    let pos = 0;
+    let deltaX = 0;
 
     // reset
     $swp.addEventListener('click', () => {
-      console.log(this.slid);
       if (this.slid) this.slid = false;
+    });
+
+    $swp.addEventListener('touchstart', (e) => {
+      startStamp = new Date().getTime();
+      startTouch = e.touches[0];
+      $swp.style.transition = 'none';
+    });
+
+    $swp.addEventListener('touchmove', (e) => {
+      const touch = e.touches[0];
+
+      if (!isHor && touch.clientY == startTouch.clientY) {
+        isHor = true;
+      }
+
+      if (!isHor) return; // 不是水平移动就不处理了
+
+      // 处理滑动
+      deltaX = touch.clientX - startTouch.clientX;
+
+      if (this.slid) {
+        pos = Math.min(Math.max(deltaX, 0), this.width) - this.width;
+      } else {
+        pos = Math.max(Math.min(deltaX, 0), -this.width);
+      }
+      $swp.style.transform = 'translateX(' + pos + 'px)';
+    });
+
+    $swp.addEventListener('touchend', (e) => {
+      const stamp = new Date().getTime();
+      const touch = e.touches[0];
+
+      // 持续时间短，考虑滑动的情况
+      if (stamp - startStamp < 300) {
+        if (deltaX > 5) {
+          $swp.style.transform = 'translateX(0)';
+          this.slid = false;
+          pos = 0;
+        } else if (deltaX < -5) {
+          $swp.style.transform = `translateX(-${this.width}px)`;
+          this.slid = true;
+          pos = `-${this.width}`;
+        }
+      } else {
+        if (Math.abs(pos) < this.minWidth) {
+          $swp.style.transform = 'translateX(0)';
+          this.slid = false;
+          pos = 0;
+        } else {
+          $swp.style.transform = `translateX(-${this.width}px)`;
+          this.slid = true;
+          pos = `-${this.width}`;
+        }
+      }
+
+      $swp.style.transition = 'transform .3s ease';
+      isHor = false;
+      startTouch = null;
+      deltaX = 0;
     });
 
   }
@@ -68,12 +137,12 @@ export default {
   height: 100%;
   transition: transform .3s ease;
 }
-.swiper-left .swp-right,
-.swiper-left .swp-left {
+.swiper-left .swp-left,
+.swiper-left .swp-right {
   height: 100%;
   float: left;
 }
-.swiper-left .swp-right {
+.swiper-left .swp-left {
   width: 100vw;
 }
 </style>
